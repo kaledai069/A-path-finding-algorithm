d1a0fb1147e75a3a4976835b0ad43ce0e1ff63f5
kaledai069 <parzival.cooler@gmail.com>
2022-08-12 19:29:12 +0545

    maze generation and solver added

--- A star visualizer.py
+++ A star visualizer.py
@@ -1,8 +1,9 @@
-from turtle import title
 import pygame
 from warnings import warn
 import heapq
 from pygame import *
+import random
+
 pygame.init()
 fps = 360
 fps_clock = pygame.time.Clock()
@@ -21,9 +22,11 @@ end_box_pos = (220, 75)
 blockers_box_pos = (380, 75)
 start_algo_btn_pos = (540, 75)
 reset_btn_pos = (1120, 75)
+maze_btn_pos = (720, 75)
 
 animation_list = []
 path = []
+maze = []
 
 node_list_complete = False
 path_list_complete = False
@@ -39,6 +42,8 @@ end_btn_clicked = False
 blockers_btn_clicked = False
 animation_completion = False
 no_path_found_state = False
+maze_created = False
+animation_started = False
 
 start_node_position = ()
 end_node_position = ()
@@ -66,6 +71,251 @@ open_list_node_fill_color = pygame.Color("#21ABA5")
 # Fonts Defined
 font_large = pygame.font.Font('Roboto-Bold.ttf', 28)
 font_small = pygame.font.Font('Roboto-Regular.ttf', 18)
+
+def generate_maze():
+    # Find number of surrounding cells
+    def surroundingCells(rand_wall):
+        s_cells = 0
+        if (maze[rand_wall[0]-1][rand_wall[1]] == 'c'):
+            s_cells += 1
+        if (maze[rand_wall[0]+1][rand_wall[1]] == 'c'):
+            s_cells += 1
+        if (maze[rand_wall[0]][rand_wall[1]-1] == 'c'):
+            s_cells +=1
+        if (maze[rand_wall[0]][rand_wall[1]+1] == 'c'):
+            s_cells += 1
+
+        return s_cells
+
+
+    ## Main code
+    # Init variables
+    wall = 'w'
+    cell = 'c'
+    unvisited = 'u'
+    height = 57
+    width = 122
+    maze = []
+
+    # Initialize colorama
+
+    # Denote all cells as unvisited
+    for i in range(0, height):
+        line = []
+        for j in range(0, width):
+            line.append(unvisited)
+        maze.append(line)
+
+    # Randomize starting point and set it a cell
+    starting_height = int(random.random()*height)
+    starting_width = int(random.random()*width)
+    if (starting_height == 0):
+        starting_height += 1
+    if (starting_height == height-1):
+        starting_height -= 1
+    if (starting_width == 0):
+        starting_width += 1
+    if (starting_width == width-1):
+        starting_width -= 1
+
+    # Mark it as cell and add surrounding walls to the list
+    maze[starting_height][starting_width] = cell
+    walls = []
+    walls.append([starting_height - 1, starting_width])
+    walls.append([starting_height, starting_width - 1])
+    walls.append([starting_height, starting_width + 1])
+    walls.append([starting_height + 1, starting_width])
+
+    # Denote walls in maze
+    maze[starting_height-1][starting_width] = 'w'
+    maze[starting_height][starting_width - 1] = 'w'
+    maze[starting_height][starting_width + 1] = 'w'
+    maze[starting_height + 1][starting_width] = 'w'
+
+    while (walls):
+        # Pick a random wall
+        rand_wall = walls[int(random.random()*len(walls))-1]
+
+        # Check if it is a left wall
+        if (rand_wall[1] != 0):
+            if (maze[rand_wall[0]][rand_wall[1]-1] == 'u' and maze[rand_wall[0]][rand_wall[1]+1] == 'c'):
+                # Find the number of surrounding cells
+                s_cells = surroundingCells(rand_wall)
+
+                if (s_cells < 2):
+                    # Denote the new path
+                    maze[rand_wall[0]][rand_wall[1]] = 'c'
+
+                    # Mark the new walls
+                    # Upper cell
+                    if (rand_wall[0] != 0):
+                        if (maze[rand_wall[0]-1][rand_wall[1]] != 'c'):
+                            maze[rand_wall[0]-1][rand_wall[1]] = 'w'
+                        if ([rand_wall[0]-1, rand_wall[1]] not in walls):
+                            walls.append([rand_wall[0]-1, rand_wall[1]])
+
+
+                    # Bottom cell
+                    if (rand_wall[0] != height-1):
+                        if (maze[rand_wall[0]+1][rand_wall[1]] != 'c'):
+                            maze[rand_wall[0]+1][rand_wall[1]] = 'w'
+                        if ([rand_wall[0]+1, rand_wall[1]] not in walls):
+                            walls.append([rand_wall[0]+1, rand_wall[1]])
+
+                    # Leftmost cell
+                    if (rand_wall[1] != 0):	
+                        if (maze[rand_wall[0]][rand_wall[1]-1] != 'c'):
+                            maze[rand_wall[0]][rand_wall[1]-1] = 'w'
+                        if ([rand_wall[0], rand_wall[1]-1] not in walls):
+                            walls.append([rand_wall[0], rand_wall[1]-1])
+                
+
+                # Delete wall
+                for wall in walls:
+                    if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
+                        walls.remove(wall)
+
+                continue
+
+        # Check if it is an upper wall
+        if (rand_wall[0] != 0):
+            if (maze[rand_wall[0]-1][rand_wall[1]] == 'u' and maze[rand_wall[0]+1][rand_wall[1]] == 'c'):
+
+                s_cells = surroundingCells(rand_wall)
+                if (s_cells < 2):
+                    # Denote the new path
+                    maze[rand_wall[0]][rand_wall[1]] = 'c'
+
+                    # Mark the new walls
+                    # Upper cell
+                    if (rand_wall[0] != 0):
+                        if (maze[rand_wall[0]-1][rand_wall[1]] != 'c'):
+                            maze[rand_wall[0]-1][rand_wall[1]] = 'w'
+                        if ([rand_wall[0]-1, rand_wall[1]] not in walls):
+                            walls.append([rand_wall[0]-1, rand_wall[1]])
+
+                    # Leftmost cell
+                    if (rand_wall[1] != 0):
+                        if (maze[rand_wall[0]][rand_wall[1]-1] != 'c'):
+                            maze[rand_wall[0]][rand_wall[1]-1] = 'w'
+                        if ([rand_wall[0], rand_wall[1]-1] not in walls):
+                            walls.append([rand_wall[0], rand_wall[1]-1])
+
+                    # Rightmost cell
+                    if (rand_wall[1] != width-1):
+                        if (maze[rand_wall[0]][rand_wall[1]+1] != 'c'):
+                            maze[rand_wall[0]][rand_wall[1]+1] = 'w'
+                        if ([rand_wall[0], rand_wall[1]+1] not in walls):
+                            walls.append([rand_wall[0], rand_wall[1]+1])
+
+                # Delete wall
+                for wall in walls:
+                    if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
+                        walls.remove(wall)
+
+                continue
+
+        # Check the bottom wall
+        if (rand_wall[0] != height-1):
+            if (maze[rand_wall[0]+1][rand_wall[1]] == 'u' and maze[rand_wall[0]-1][rand_wall[1]] == 'c'):
+
+                s_cells = surroundingCells(rand_wall)
+                if (s_cells < 2):
+                    # Denote the new path
+                    maze[rand_wall[0]][rand_wall[1]] = 'c'
+
+                    # Mark the new walls
+                    if (rand_wall[0] != height-1):
+                        if (maze[rand_wall[0]+1][rand_wall[1]] != 'c'):
+                            maze[rand_wall[0]+1][rand_wall[1]] = 'w'
+                        if ([rand_wall[0]+1, rand_wall[1]] not in walls):
+                            walls.append([rand_wall[0]+1, rand_wall[1]])
+                    if (rand_wall[1] != 0):
+                        if (maze[rand_wall[0]][rand_wall[1]-1] != 'c'):
+                            maze[rand_wall[0]][rand_wall[1]-1] = 'w'
+                        if ([rand_wall[0], rand_wall[1]-1] not in walls):
+                            walls.append([rand_wall[0], rand_wall[1]-1])
+                    if (rand_wall[1] != width-1):
+                        if (maze[rand_wall[0]][rand_wall[1]+1] != 'c'):
+                            maze[rand_wall[0]][rand_wall[1]+1] = 'w'
+                        if ([rand_wall[0], rand_wall[1]+1] not in walls):
+                            walls.append([rand_wall[0], rand_wall[1]+1])
+
+                # Delete wall
+                for wall in walls:
+                    if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
+                        walls.remove(wall)
+
+
+                continue
+
+        # Check the right wall
+        if (rand_wall[1] != width-1):
+            if (maze[rand_wall[0]][rand_wall[1]+1] == 'u' and maze[rand_wall[0]][rand_wall[1]-1] == 'c'):
+
+                s_cells = surroundingCells(rand_wall)
+                if (s_cells < 2):
+                    # Denote the new path
+                    maze[rand_wall[0]][rand_wall[1]] = 'c'
+
+                    # Mark the new walls
+                    if (rand_wall[1] != width-1):
+                        if (maze[rand_wall[0]][rand_wall[1]+1] != 'c'):
+                            maze[rand_wall[0]][rand_wall[1]+1] = 'w'
+                        if ([rand_wall[0], rand_wall[1]+1] not in walls):
+                            walls.append([rand_wall[0], rand_wall[1]+1])
+                    if (rand_wall[0] != height-1):
+                        if (maze[rand_wall[0]+1][rand_wall[1]] != 'c'):
+                            maze[rand_wall[0]+1][rand_wall[1]] = 'w'
+                        if ([rand_wall[0]+1, rand_wall[1]] not in walls):
+                            walls.append([rand_wall[0]+1, rand_wall[1]])
+                    if (rand_wall[0] != 0):	
+                        if (maze[rand_wall[0]-1][rand_wall[1]] != 'c'):
+                            maze[rand_wall[0]-1][rand_wall[1]] = 'w'
+                        if ([rand_wall[0]-1, rand_wall[1]] not in walls):
+                            walls.append([rand_wall[0]-1, rand_wall[1]])
+
+                # Delete wall
+                for wall in walls:
+                    if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
+                        walls.remove(wall)
+
+                continue
+
+        # Delete the wall from the list anyway
+        for wall in walls:
+            if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
+                walls.remove(wall)
+        
+
+
+    # Mark the remaining unvisited cells as walls
+    for i in range(0, height):
+        for j in range(0, width):
+            if (maze[i][j] == 'u'):
+                maze[i][j] = 'w'
+
+    # Set entrance and exit
+    for i in range(0, width):
+        if (maze[1][i] == 'c'):
+            maze[0][i] = 'c'
+            break
+
+    for i in range(width-1, 0, -1):
+        if (maze[height-2][i] == 'c'):
+            maze[height-1][i] = 'c'
+            break
+
+
+
+    new_maze = [[0 for i in range(width)] for j in range(height)]
+    for i in range(0, height):
+        for j in range(0, width):
+            if maze[i][j] == 'w':
+                new_maze[i][j] = 1
+    return new_maze
+
+
 
 class Node():
     def __init__(self, parent = None, position = None):
@@ -129,7 +379,7 @@ def draw_rects():
     l_x = 13
     l_y = 131
     outer_shell_rect = (9, 127, 1349, 634)
-    pygame.draw.rect(screen, title_color, outer_shell_rect, width = 4, border_radius=2)
+    pygame.draw.rect(screen, white, outer_shell_rect, width = 4, border_radius = 3)
     for i in range(rows):
         for j in range(cols):
             box_arrays.append((l_x, l_y))
@@ -151,6 +401,16 @@ def mouse_event_handler(cur_pos, color = block_rect_color, other_node = False):
             if blockers_btn_state:
                 cur_index_value = convert_pos_to_array_index(cur_pos)
                 box_node_array[cur_index_value[0]][cur_index_value[1]].node_type = 0
+
+def create_maze_btn():
+    global maze_btn_pos
+    maze_btn_w = 120
+    maze_btn_h = 30
+    maze_btn_text = font_small.render("Create Maze", True, white)
+
+    pygame.draw.rect(screen, pygame.Color(235, 20, 20), (maze_btn_pos[0], maze_btn_pos[1], maze_btn_w, maze_btn_h), width = 0, border_radius = 8)
+    screen.blit(maze_btn_text, (maze_btn_pos[0] + maze_btn_w / 2 - maze_btn_text.get_width() / 2, maze_btn_pos[1] + maze_btn_h / 2 - maze_btn_text.get_height() / 2))
+
 
 def reset_btn_logic():
     global reset_btn_pos
@@ -209,10 +469,13 @@ def btn_classifier(click_pos):
     global end_btn_clicked
     global path
     global node_list_complete
+    global animation_started
+    global maze_created
 
     if contained(click_pos, start_box_pos, (120, 35)) and not start_btn_clicked:
         global start_btn_state
 
+        
         start_btn_state = not start_btn_state
         start_btn_clicked = True
         if start_btn_state:
@@ -230,7 +493,7 @@ def btn_classifier(click_pos):
         else:
             end_box_node(orange_dark_col)
 
-    elif contained(click_pos, blockers_box_pos, (120, 35)) and start_btn_clicked and end_btn_clicked:
+    elif contained(click_pos, blockers_box_pos, (120, 35)) and start_btn_clicked and end_btn_clicked and not animation_started:
         global blockers_btn_clicked
         global blockers_btn_state
         global start_algo_btn_state
@@ -247,12 +510,48 @@ def btn_classifier(click_pos):
                 start_algo_btn_state = True
     
     elif contained(click_pos, start_algo_btn_pos, (160, 35)) and start_algo_btn_state:
+        animation_started = True
         path = run_a_star_algorithm()
         node_list_complete = True
 
     elif contained(click_pos, reset_btn_pos, (120, 30)):
         reset_all_vals()
-   
+        
+    elif contained(click_pos, maze_btn_pos, (120, 30)) and not maze_created and not start_btn_clicked:
+        start_algo_btn(start_algo_light_col)
+        start_algo_btn_state = True
+        animation_started = True
+        create_maze()
+
+def create_maze():
+    global maze_created
+    global maze
+    global start_btn_clicked
+    global end_btn_clicked 
+    global start_node_position
+    global end_node_position
+    global rows
+    global cols
+
+    maze_created = True
+    start_node_position = (0, 1)
+
+    start_btn_clicked = True
+    end_btn_clicked = True
+    maze = generate_maze()
+    for i in range(rows):
+        for j in range(cols):
+            if maze[i][j] == 1:
+                pygame.draw.rect(screen, pygame.Color(40,40,40), (box_node_array[i][j].xdist_position - 0.5, box_node_array[i][j].ydist_position - 0.5, 11, 11))
+    if maze[56][120] == 1:
+        end_node_position = (56, 119)
+    else:
+        end_node_position = (56, 120)
+
+    pygame.draw.rect(screen, start_green_col, (24 - 0.5, 131 - 0.5, 11, 11))
+    pygame.draw.rect(screen, orange_col, (end_node_position[1] * 11 + 13 - 0.5, end_node_position[0] * 11 + 131 - 0.5, 11, 11))
+
+
 def reset_all_vals():
     global box_arrays 
     global animation_list
@@ -275,10 +574,14 @@ def reset_all_vals():
     global path_index
     global path_time_gap 
     global no_path_found_state
+    global maze_created
+    global animation_started
+    global maze
 
     box_arrays = []
     animation_list = []
     path = []
+    maze = []
     node_list_complete = False
     path_list_complete = False
     mouse_motion = False
@@ -295,8 +598,11 @@ def reset_all_vals():
     box_node_array = [[Node() for i in range(cols)] for j in range(rows)]
     animation_index = 0
     path_index = 0
-    path_time_gap = 1
+    path_time_gap = 5
     no_path_found_state = False
+    maze_created = False
+    animation_started = False
+
     draw_basic_UIs()
 
 
@@ -310,6 +616,7 @@ def draw_basic_UIs():
     blockers_box(off_white)
     start_algo_btn(start_algo_dark_col)
     reset_btn_logic()
+    create_maze_btn()
 
 def draw_green_node(clicked_pos):
     global start_btn_state
@@ -351,6 +658,7 @@ def calculate_val(pos):
     return pos[0] + pos[1] * cols 
 
 def run_a_star_algorithm(allow_diagonal_movement = False):
+    global maze
     global start_node_position
     global end_node_position
 
@@ -361,12 +669,13 @@ def run_a_star_algorithm(allow_diagonal_movement = False):
     global rows
     global animation_list
 
-    maze = [[0 for i in range(cols)] for j in range(rows)]
+    if not maze_created:
+        maze = [[0 for i in range(cols)] for j in range(rows)]
 
-    for i in range(rows):
-        for j in range(cols):
-            if box_node_array[i][j].node_type == 0:
-                maze[i][j] = 1
+        for i in range(rows):
+            for j in range(cols):
+                if box_node_array[i][j].node_type == 0:
+                    maze[i][j] = 1
 
     start_node = Node(None, start_index)
     start_node.g = start_node.h = start_node.f = 0
@@ -377,7 +686,6 @@ def run_a_star_algorithm(allow_diagonal_movement = False):
     # Initialize both open and closed list
     open_list = []
     closed_list = []
-
 
     # Heapify the open_list and Add the start node
     heapq.heapify(open_list) 
@@ -406,6 +714,11 @@ def run_a_star_algorithm(allow_diagonal_movement = False):
         
         # Get the current node
         current_node = heapq.heappop(open_list)
+        if current_node.position is not None:
+            if current_node.position[0] == end_index[0] and current_node.position[1] == end_index[1]:
+                print("End node found")
+
+            
         closed_list.append(current_node)
         box_node_array[current_node.position[0]][current_node.position[1]].in_open_list = False
         box_node_array[current_node.position[0]][current_node.position[1]].in_close_list = True
@@ -475,14 +788,14 @@ def no_path_found():
     global no_path_found_state 
     no_path_found_state = True
     no_path_text = font_large.render("No Path Found", True, white)
-    screen.blit(no_path_text, (820, 70))
+    screen.blit(no_path_text, (880, 75))
 
 draw_basic_UIs()
 
 animation_index = 0
 path_index = 0
 time_gap = -1
-path_time_gap = 1
+path_time_gap = 5
 
 while running:
     for event in pygame.event.get():
@@ -515,15 +828,15 @@ while running:
         if animation_index + 1 == len(animation_list):
             node_list_complete = False
             path_list_complete = True
-            pygame.draw.rect(screen, pygame.Color(235,40,40), (start_node_position[1] * 11 + 13 - 0.5, start_node_position[0] * 11 + 131 - 0.5, rect_width + 2, rect_height + 2))
+            pygame.draw.rect(screen, start_green_dark_col, (start_node_position[1] * 11 + 13 - 0.5, start_node_position[0] * 11 + 131 - 0.5, rect_width + 2, rect_height + 2))
         else:
             animation_index += 1
 
     if path_list_complete and not no_path_found_state:
         if path_time_gap < 0:
-            path_time_gap = 1
+            path_time_gap = 5
             if path_index + 1 != len(path):
-                pygame.draw.line(screen, pygame.Color(235, 40, 40), (path[path_index][1] * 11 + 13 + 5, path[path_index][0] * 11 + 131 + 5), (path[path_index + 1][1] * 11 + 13 + 5, path[path_index + 1][0] * 11 + 131 + 5), 3)
+                pygame.draw.line(screen, pygame.Color(235, 40, 40), (path[path_index][1] * 11 + 13 + 4, path[path_index][0] * 11 + 131 + 4), (path[path_index + 1][1] * 11 + 13 + 4, path[path_index + 1][0] * 11 + 131 + 4), 3)
 
             if path_index + 1 == len(path):
                 path_list_complete = False
